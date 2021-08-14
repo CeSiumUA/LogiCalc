ccfcc25530e622dd9feb77d414caa57de7f54696
Fedir Katushonok <mtg.in.ua@gmail.com>
2021-08-14 13:43:15 +0300

    init

---
+++ LogiCalc/Models/Dispoplan.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogiCalc.Models
+{
+    public class Dispoplan
+    {
+        public List<RouteCoefficient> Routes { get; set; } = new List<RouteCoefficient>();
+        public DateTime ActualBy { get; set; } = DateTime.Now;
+        public void FillPlan(string rawText)
+        {
+
+        }
+        public static Dispoplan CreateDispoplan(string rawText)
+        {
+            Dispoplan dispoplan = new Dispoplan();
+            dispoplan.FillPlan(rawText);
+            return dispoplan;
+        }
+    }
+}
---
+++ LogiCalc/Models/RouteCoefficient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogiCalc.Models
+{
+    public record RouteCoefficient
+    {
+        public double Minimum { get; set; } = 0;
+        public double Maximum { get; set; } = 0;
+        public string From { get; set; } = string.Empty;
+        public string To { get; set; } = string.Empty;
+    }
+}
