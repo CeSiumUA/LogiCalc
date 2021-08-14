bf6ecbba6af3727b95465209486da8644040bed3
Fedir Katushonok <mtg.in.ua@gmail.com>
2021-08-14 15:26:05 +0300

    Added combobox bindings

--- LogiCalc.Core/ViewModels/MainViewModel.cs
+++ LogiCalc.Core/ViewModels/MainViewModel.cs
@@ -11,6 +11,46 @@ namespace LogiCalc.Core.ViewModels
     public class MainViewModel : INotifyPropertyChanged
     {
         public Dispoplan Dispoplan { get; set; }
+        public string[] Froms
+        {
+            get
+            {
+                return this.Dispoplan.Routes.Select(x => x.From).Distinct().OrderBy(x => x).ToArray();
+            }
+        }
+        public string[] Tos
+        {
+            get
+            {
+                return this.Dispoplan.Routes.Select(x => x.To).Distinct().OrderBy(x => x).ToArray();
+            }
+        }
+        private string selectedFrom;
+        public string SelectedFrom
+        {
+            get
+            {
+                return selectedFrom;
+            }
+            set
+            {
+                this.selectedFrom = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SelectedFrom"));
+            }
+        }
+        private string selectedTo;
+        public string SelectedTo
+        {
+            get
+            {
+                return selectedTo;
+            }
+            set
+            {
+                this.selectedTo = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SelectedTo"));
+            }
+        }
         public MainViewModel()
         {
             this.Dispoplan = new Dispoplan();
@@ -21,6 +61,9 @@ namespace LogiCalc.Core.ViewModels
             {
                 this.Dispoplan.FillPlan(rawText);
             }
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Froms"));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Tos"));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Dispoplan"));
         }
         public event PropertyChangedEventHandler? PropertyChanged;
     }
